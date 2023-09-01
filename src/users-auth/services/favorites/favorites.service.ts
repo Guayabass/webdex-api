e4a4dac91d2601ace6ebab2687cd232741b684ff
Guayabas <guayabas0399@gmail.com>
2023-09-01 07:36:04 -0600

    test 11

--- src/users-auth/services/favorites/favorites.service.ts
+++ src/users-auth/services/favorites/favorites.service.ts
@@ -28,8 +28,8 @@ export class FavoritesService {
     newFavorite.user = user;
     newFavorite.pokemonID = favDTO.pokemonID;
     newFavorite.pokemonName = favDTO.pokemonName;
-    const existsFav = await this.favoriteRepository.findOne({ where: {user: {id: id}, pokemonID: newFavorite.pokemonID}})
-    if (existsFav){
+    const existsFav = this.getUsersFavorites(favDTO.user, favDTO.pokemonID)
+    if (existsFav){//llamar getUsersFavorites
       return new HttpException('User/Pokemon already exists', HttpStatus.CONFLICT);
     }
     return this.favoriteRepository.save(newFavorite);
